3f22849b06f2981f15437f741860561119eebdae
kolp8588 <67845932+kolp8588@users.noreply.github.com>
2020-10-29 19:06:16 +0900

    Create presenter.js

---
+++ src/screens/SignUpScreen/presenter.js
@@ -0,0 +1,138 @@
+import React from "react";
+import PropTypes from "prop-types";
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  Dimensions,
+  TouchableOpacity,
+  TextInput,
+  StatusBar,
+  ActivityIndicator,
+} from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { COLORS } from "../../constants";
+const { width, height } = Dimensions.get("window");
+
+const SignUpScreen = (props) => (
+  <View style={styles.container}>
+    <View style={styles.header}>
+      <Image
+        source={require("../../../assets/images/logo_login.png")}
+        resizeMode="stretch"
+        style={styles.logo}
+      />
+    </View>
+    <View style={styles.content}>
+      <TextInput
+        placeholder="Email"
+        style={styles.textInput}
+        autoCapitalize={"none"}
+        autoCorrect={false}
+        value={props.username}
+        onChangeText={props.changeUsername}
+      />
+      <TextInput
+        placeholder="Password"
+        style={styles.textInput}
+        autoCapitalize={"none"}
+        secureTextEntry={true}
+        value={props.password}
+        onChangeText={props.changePassword}
+        returnKeyType={"send"}
+        onSubmitEditing={props.submit}
+      />
+      <TextInput
+        placeholder="Password Check"
+        style={styles.textInput}
+        autoCapitalize={"none"}
+        secureTextEntry={true}
+        value={props.passwordCheck}
+        onChangeText={props.changePasswordCheck}
+        returnKeyType={"send"}
+        onSubmitEditing={props.submit}
+      />
+      <TouchableOpacity style={styles.touchable} onPressOut={props.submit}>
+        <View style={styles.button}>
+          {props.isSubmitting ? (
+            <ActivityIndicator size="small" color="white" />
+          ) : (
+            <Text style={styles.btnText}>회원가입</Text>
+          )}
+        </View>
+      </TouchableOpacity>      
+    </View>
+  </View>
+);
+
+SignUpScreen.propTypes = {};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  header: {
+    flex: 2,
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
+    width,
+  },
+  logo: {
+    width: 250,
+    resizeMode: "contain",
+    marginTop: 50,
+  },
+  content: {
+    flex: 4,
+    backgroundColor: "white",
+    paddingTop: 50,
+    alignItems: "center",
+    justifyContent: "flex-start",
+  },
+  fbContainer: {
+    marginTop: 30,
+  },
+  fbView: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  fbText: {
+    color: "gray",
+    marginLeft: 10,
+    fontWeight: "600",
+    fontSize: 14,
+  },
+  textInput: {
+    height: 50,
+    borderColor: "#bbb",
+    borderWidth: StyleSheet.hairlineWidth,
+    width: width - 80,
+    borderRadius: 5,
+    marginBottom: 15,
+    paddingHorizontal: 15,
+    backgroundColor: "#FAFAFA",
+    fontSize: 14,
+  },
+  touchable: {
+    borderRadius: 5,
+    backgroundColor: "#3E99EE",
+    width: width - 80,
+    marginTop: 25,
+  },
+  button: {
+    paddingHorizontal: 7,
+    backgroundColor: COLORS.DALGRAK,
+    height: 50,
+    justifyContent: "center",
+  },
+  btnText: {
+    color: "white",
+    fontWeight: "600",
+    textAlign: "center",
+    fontSize: 14,
+  },
+});
+
+export default SignUpScreen;
