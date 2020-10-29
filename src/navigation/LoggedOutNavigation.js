c39f68d44d711ce8e7220f291edb3d0e08eeeef9
kolp8588 <67845932+kolp8588@users.noreply.github.com>
2020-10-29 19:04:00 +0900

    Update LoggedOutNavigation.js

--- src/navigation/LoggedOutNavigation.js
+++ src/navigation/LoggedOutNavigation.js
@@ -1,7 +1,9 @@
 import React from "react";
+import { View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import LogInScreen from "../screens/LogInScreen";
+import SignUpScreen from "../screens/SignUpScreen";
 
 const Stack = createStackNavigator();
 
@@ -14,6 +16,14 @@ function LoggedOutNavigation() {
           component={LogInScreen}
           options={{
             headerTitle: false,
+          }}
+        />
+        <Stack.Screen
+          name="SignUp"
+          component={SignUpScreen}
+          options={{
+            headerTitle: true,
+            // headerLeft: () => <View />,
           }}
         />
       </Stack.Navigator>
