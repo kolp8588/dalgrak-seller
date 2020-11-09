b2bb5a3084743e62959d85d0e2d0f81cac46c8c2
kolp8588 <67845932+kolp8588@users.noreply.github.com>
2020-11-09 15:50:22 +0900

    Update App.js

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Image, Dimensions, View, StyleSheet } from 'react-native';
+import * as Permissions from 'expo-permissions';
+import * as Notifications from 'expo-notifications';
+import Constants from 'expo-constants';
 import * as SplashScreen from 'expo-splash-screen';
 import { Asset } from 'expo-asset';
 import { YellowBox } from "react-native";
@@ -27,6 +30,7 @@ export default class App extends React.Component {
 
   componentDidMount() {
     SplashScreen.preventAutoHideAsync();
+    this._registerForPushNotificationsAsync();
   }
 
   render() {
@@ -51,6 +55,35 @@ export default class App extends React.Component {
       </Provider>
     );
   }
+  _registerForPushNotificationsAsync = async () => {
+    if (Constants.isDevice) {
+      const { status: existingStatus } = await Permissions.getAsync(Permissions.NOTIFICATIONS);
+      let finalStatus = existingStatus;
+      if (existingStatus !== 'granted') {
+        const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+        finalStatus = status;
+      }
+      // if (finalStatus !== 'granted') {
+      //   alert('Failed to get push token for push notification!');
+      //   return;
+      // }
+      const token = (await Notifications.getExpoPushTokenAsync()).data;
+      console.log(token);
+      this.setState({ expoPushToken: token });
+    } else {
+      alert('Must use physical device for Push Notifications');
+    }
+  
+    if (Platform.OS === 'android') {
+      Notifications.setNotificationChannelAsync('default', {
+        name: 'default',
+        importance: Notifications.AndroidImportance.MAX,
+        vibrationPattern: [0, 250, 250, 250],
+        lightColor: '#FF231F7C',
+      });
+    }
+  };
+  
 
   _cacheSplashResourcesAsync = async () => {
     const gif = require('./assets/images/Logo.gif');
