56feb86a3af4f470fb301ae449e00f0562a3b5e2
kolp8588 <67845932+kolp8588@users.noreply.github.com>
2020-11-10 17:36:30 +0900

    Update index.js

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -2,15 +2,6 @@ import { connect } from "react-redux";
 import Container from "./container";
 import { actionCreators as userActions } from "../../redux/modules/user";
 
-const mapStateToProps = (state, ownProps) => {
-  const {
-    user: { profile },
-  } = state;
-  return {
-    profile,
-  };
-};
-
 const mapDispatchToProps = (dispatch, ownProps) => {
   return {
     logOut: () => {
@@ -19,4 +10,4 @@ const mapDispatchToProps = (dispatch, ownProps) => {
   };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Container);
+export default connect(null, mapDispatchToProps)(Container);
