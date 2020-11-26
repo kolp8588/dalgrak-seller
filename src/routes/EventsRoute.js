243f3596134bebb4fc4a3ad710bb05faa52f0f9f
kolp8588 <67845932+kolp8588@users.noreply.github.com>
2020-11-26 19:07:17 +0900

    Update EventsRoute.js

--- src/routes/EventsRoute.js
+++ src/routes/EventsRoute.js
@@ -1,23 +1,49 @@
 import React from "react";
+import { Component } from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import { COLORS } from "../constants"
 import EventsScreen from "../screens/EventsScreen";
+import EventOptionScreen from "../screens/EventOptionScreen";
+import CategoryScreen from "../screens/CategoryScreen";
 
 const Stack = createStackNavigator();
-function EventsRoute() {
-  return (
-    <Stack.Navigator>
-      <Stack.Screen
-        name="Events"
-        component={EventsScreen}
-        options={{
-          headerStyle: {
-            backgroundColor: COLORS.DALGRAK,
-          },
-        }}
-      />
-    </Stack.Navigator>
-  );
+class EventsRoute extends Component {
+  render() {
+    const { navigation } = this.props;
+
+    return (
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Events"
+          component={EventsScreen}
+          options={{
+            headerStyle: {
+              backgroundColor: COLORS.DALGRAK,
+            },
+          }}
+        />
+        <Stack.Screen
+          name="EventOption"
+          component={EventOptionScreen}
+          options={{
+            headerStyle: {
+              backgroundColor: COLORS.DALGRAK,
+            },
+          }}
+        />
+        <Stack.Screen
+          name="Category"
+          component={CategoryScreen}
+          options={{
+            headerStyle: {
+              backgroundColor: COLORS.DALGRAK,
+            },
+            headerTintColor: "white",
+          }}
+        />
+      </Stack.Navigator>
+    );
+  }
 }
 
 export default EventsRoute;
