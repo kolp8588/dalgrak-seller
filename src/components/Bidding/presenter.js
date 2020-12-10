c57bb108804886411ba4c48442313d8de5b4f126
kolp8588 <67845932+kolp8588@users.noreply.github.com>
2020-12-10 19:58:46 +0900

    Update presenter.js

--- src/components/Bidding/presenter.js
+++ src/components/Bidding/presenter.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import { View, Text, Image, Dimensions, StyleSheet } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import FadeIn from "react-native-fade-in-image";
+import CountDown from "react-native-countdown-component";
 import { COLORS, FONTS } from "../../constants";
 import { useNavigation } from "@react-navigation/native";
 
@@ -11,8 +12,13 @@ const { width, height } = Dimensions.get("window");
 class Bidding extends Component {
   render() {
     const { navigation } = this.props;
-    var bidding = this.props.bidding;
-    var dalgrak = bidding.dalgrak;
+    let bidding = this.props.bidding;
+    let dalgrak = bidding.dalgrak;
+
+    let curDate = new Date().getTime();
+    let endDate = this.props.bidding.dalgrak.date;
+    let sec = (endDate - curDate) / 1000;
+
     return (
       <View style={styles.dalgrak}>
         <TouchableOpacity
@@ -35,14 +41,31 @@ class Bidding extends Component {
                 marginVertical: 2,
               }}
             >
-              <Text
-                style={{
-                  fontSize: FONTS.SIZE.TITLE,
-                  color: COLORS.DALGRAK,
-                }}
-              >
-                {dalgrak.category}
-              </Text>
+              <View style={{flexDirection:"row"}}>
+                <Text
+                  style={{
+                    fontSize: FONTS.SIZE.TITLE,
+                    color: COLORS.DALGRAK,
+                  }}
+                >
+                  {dalgrak.category}
+                </Text>
+                <CountDown
+                  style={{marginLeft: 5}}
+                  until={sec}
+                  size={12}
+                  digitTxtStyle={{ color: COLORS.DALGRAK }}
+                  digitStyle={{ 
+                    borderWidth: 2,
+                    borderColor: COLORS.DALGRAK,
+                    backgroundColor: "white" 
+                  }}
+                  timeToShow={['H', 'M', 'S']}
+                  showSeparator={true}
+                  timeLabels={{h: null, m: null, s: null}}
+                />
+              </View>
+
               <Text
                 style={{
                   marginTop: 3,
@@ -50,13 +73,6 @@ class Bidding extends Component {
                 }}
               >
                 {dalgrak.quantity} {dalgrak.unit}
-              </Text>
-              <Text
-                style={{
-                  fontSize: FONTS.SIZE.CONTENTS,
-                }}
-              >
-                마감일 : {this.props.parseDate(dalgrak.date)}
               </Text>
               <Text
                 style={{
