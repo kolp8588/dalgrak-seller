9774978bea4d8f331d7bf99e98c7aa5ff4e5c516
kolp8588 <67845932+kolp8588@users.noreply.github.com>
2020-12-17 19:34:14 +0900

    Update container.js

--- src/screens/RelatedDalgrakScreen/container.js
+++ src/screens/RelatedDalgrakScreen/container.js
@@ -2,9 +2,43 @@ import React, { Component } from "react";
 import RelatedDalgrakScreen from "./presenter";
 
 class Container extends Component {
+  state = {
+    isModalVisible: false,
+    isSubmitting: false,
+  };
   render() {
-    return <RelatedDalgrakScreen {...this.state} {...this.props} />;
+    return <RelatedDalgrakScreen 
+      {...this.state} 
+      {...this.props} 
+      submit={this.submit}
+      onPressSubmit={this.onPressSubmit}
+    />;
   }
+  onPressSubmit = (bool) => {
+    this.setState({
+      isModalVisible: bool,
+    });
+  };
+
+  submit = async () => {
+    const {
+      route: {
+        params: { bidding },
+      },
+      submit,
+    } = this.props;
+
+    this.setState({
+      isSubmitting: true,
+    });
+    if (await submit(bidding.id)) {
+      this.setState({
+        isSubmitting: false,
+        isModalVisible: false,
+      });
+      this.props.navigation.goBack();
+    }
+  };
 }
 
 export default Container;
