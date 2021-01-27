eb975e7e9b2051bd83cd204bb5b75720f038e6a7
kolp8588 <67845932+kolp8588@users.noreply.github.com>
2021-01-27 18:58:26 +0900

    Update container.js

--- src/components/Request/container.js
+++ src/components/Request/container.js
@@ -1,12 +1,56 @@
-import { connect } from "react-redux";
+import React from "react";
+import {Component} from "react";
 import Request from "./presenter";
 
-const mapStateToProps = (state, ownProps) => {
-  return {};
-};
+class Container extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      interestId: "",
+      isInterest: false
+    };
+  }
+  static propTypes = {};
 
-const mapDispatchToProps = (dispatch, ownProps) => {
-  return {};
-};
+  componentDidMount() {
+    console.log("CHECK :")
+    console.log(this.props)
+    if (this.props.interestId != undefined) {
+      this.setState({
+        interestId: this.props.interestId,
+        isInterest: true,
+      });
+    }
+  }
+  render() {
+    return (
+      <Request 
+        {...this.props} 
+        {...this.state}
+        submit={this.onSubmit} />
+    );
+  }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Request);
+  onSubmit = async () => {
+    if (!this.state.isInterest) {
+      const { submit } = this.props; 
+      const request = {
+        dalgrakId : this.props.id
+      }   
+      const interestId = await submit(request);
+      this.setState({
+        isInterest: true,
+        interestId: interestId
+      })
+    } else {
+      const { remove } = this.props; 
+      await remove(this.state.interestId);
+      this.setState({
+        isInterest: false,
+        interestId: ""
+      })
+    }
+  }
+}
+
+export default Container;
