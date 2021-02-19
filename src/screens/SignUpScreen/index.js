2f785c162eabe556f722346a99aff358d2728e3e
hose <gaejin830@gmail.com>
2021-02-19 19:57:23 +0900

    Update index.js

--- src/screens/SignUpScreen/index.js
+++ src/screens/SignUpScreen/index.js
@@ -1,8 +1,21 @@
 import { connect } from "react-redux";
 import Container from "./container";
 
+import { actionCreators as userActions } from "../../redux/modules/user";
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+  };
+};
+
 const mapDispatchToProps = (dispatch, ownProps) => {
   return {
+    emailDupCheck: (email) => {
+      return dispatch(userActions.emailDupCheck(email));
+    },
+    usernameDupCheck: (username) => {
+      return dispatch(userActions.usernameDupCheck(username));
+    },
   };
 };
 
